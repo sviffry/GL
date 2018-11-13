3cf7940434cf38b35d83b59ee368a5b9dcb7e4f0
sasha viffry <sasha.viffry@ge1.iut-cachan.u-psud.fr>
2018-11-13 11:31:31 +0100

    fini la carrection du DS exo 2

--- include/my_header.h
+++ include/my_header.h
@@ -1,9 +1,26 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#ifndef MY_HEADER_H
-#define MY_HEADER_H
+#include "my_header.h"
+#include <math.h>
 
+#define ECART 1e-10
 
+int main(void)
+{
+    double a = 1.0, b = 2.0, c = 3.0;
+    double delta;
+    unsigned char nb_solutions;
 
-#endif
+    delta = b*b - 4.0 * a *c;
+
+    if ( fabs(delta) <  ECART )
+        nb_solutions = 1;
+    else if( delta > 0)
+        nb_solutions = 2;
+    else
+        nb_solutions = 0;
+
+    return 0;
+}
+
