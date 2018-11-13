9bca27e3c8ce12b4d13b689aaf3d006beaf022e2
sasha viffry <sasha.viffry@ge1.iut-cachan.u-psud.fr>
2018-11-13 11:44:26 +0100

    fini correction exo 3 DS

--- include/my_header.h
+++ include/my_header.h
@@ -1,9 +1,18 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#ifndef MY_HEADER_H
-#define MY_HEADER_H
+#include "my_header.h"
 
+int main(void)
+{
+    unsigned long long int S1 = 1;
+    unsigned char n = 2;
 
+    while (n<50)
+    {
+        S1 = S1 * n;
+        n = n + 1;
+    }
 
-#endif
+    return 0;
+}
