1f28e1dcf6b53113b502cce0afb4b86558716da9
sviffry <44799549+sviffry@users.noreply.github.com>
2018-11-15 12:33:34 +0100

    Je commence exo 43

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-
 #include "my_header.h"
 
 int main(void)
+
 {
+    unsigned long long S1=1;
+    unsigned long n=3;
+
+    while(n<9876)
+    {
+        S1=S1*n;
+        n=n+3;
+    }
     return 0;
 }
